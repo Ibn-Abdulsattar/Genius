19b90527eb73c5d2916c5d8e1e242d1cdd80a3df
Usman Chohan <attariusman813@gmail.com>
2025-12-03 11:11:50 +0500

    structure improve for reusable component

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -4,13 +4,12 @@ import Dashboard_Stats from "../../data/dashboard.js";
 import DocsTitle from "../../components/DocsTitle.jsx";
 
 export default function Dashboard() {
-
   return (
     <div className="page-content font-serif w-full">
       <div className="dashboard bg-white rounded-3xl max-h-[77.9vh] side-scroll overflow-x-hidden overflow-y-auto">
         {/* All Content */}
         <div className="All-Content">
-          <div className="flex flex-col overflow-y-auto relative lg:flex-row justify-between items-start gap-6 lg:gap-8">
+          <div className="flex flex-col relative lg:flex-row justify-between items-start gap-6 lg:gap-8">
             {/* Left Section */}
             <div className="dashboard-left w-full lg:w-1/2">
               {/* Profile */}
@@ -24,21 +23,19 @@ export default function Dashboard() {
                 </div>
 
                 <div className="flex-1 min-w-0">
-                  <h2 className="font-bold tracking-[0.16px] text-lg sm:text-xl text-gray-800 truncate">
+                  <h2 className="font-medium tracking-[0.16px] text-lg sm:text-2xl md:text-3xl text-gray-800 truncate">
                     Welcome Christina Dykes
                   </h2>
-                  <p className="text-sm sm:text-md font-normal text-gray-500">
+                  <p className="text-sm truncate sm:text-lg font-normal font-[lato] tracking-wide sm:tracking-wider md:tracking-[.19rem] text-gray-500">
                     Discover the Statistics of Your Genius™
                   </p>
                 </div>
               </div>
-
-              <h3 className="text-xl dashboard-title sm:text-2xl font-semibold text-gray-700 border-b-2 border-[#FED302] inline-block ">
-                Dashboard
-              </h3>
+              {/* Title */}
+              <DocsTitle title="Dashboard" />
 
               {/* Stats Grid - on mobile */}
-              <div className="block lg:hidden mt-6">
+              <div className="block lg:hidden">
                 <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 gap-4 sm:gap-6">
                   {Dashboard_Stats.map((item, index) => (
                     <Card key={index} title={item.title} num={item.num} />
@@ -58,7 +55,7 @@ export default function Dashboard() {
               </div>
             </div>
             {/* Dashboard Stats  - Desktop */}
-            <div className="hidden dashboard-stats lg:block absolute bottom-10 left-10 w-[91%] lg:h-[57%] ">
+            <div className="hidden dashboard-stats lg:block absolute bottom-7 left-10 w-[91%] lg:h-[57%] ">
               <div className="grid grid-cols-3 gap-5">
                 {Dashboard_Stats.map((item, index) => (
                   <Card key={index} title={item.title} num={item.num} />
